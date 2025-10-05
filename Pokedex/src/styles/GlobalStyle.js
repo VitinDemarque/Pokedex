8880e7b108b055138d6c507ff9fe6a16ab214331
Victor Vieira Demarque <victordeemarque7@gmail.com>
2025-10-05 19:28:55 -0300

    Recriando Main.jsx e Criando StyleGlobal
    Recriando Main.jsx e Fazendo um StyleGlobal para o trabalho todo

---
+++ Pokedex/src/styles/GlobalStyle.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.fonts.main};
+    color: ${({ theme }) => theme.colors.text};
+    background: linear-gradient(135deg, #6e7b8b 0%, #2c3e50 100%);
+    background-attachment: fixed;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
